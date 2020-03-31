c7af1eb401b33aae7950bf315418361452e2484f
Neve Foresti <neve.foresti@gmail.com>
2020-03-30 17:14:57 -0700

    build files

---
+++ build/precache-manifest.54ca87f63f9ea6ffea54fddf3c45221b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4b1aa98c203dff88e62c6c20a87301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0352f00a55553bd4ab5e",
+    "url": "/static/css/main.1e8a6e25.chunk.css"
+  },
+  {
+    "revision": "e28117febf40751e0270",
+    "url": "/static/js/2.d07c2b6e.chunk.js"
+  },
+  {
+    "revision": "d87ea7904df5be32902c229dda011267",
+    "url": "/static/js/2.d07c2b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0352f00a55553bd4ab5e",
+    "url": "/static/js/main.5ad05792.chunk.js"
+  },
+  {
+    "revision": "58bc9d6695840bab33d8",
+    "url": "/static/js/runtime-main.f904433d.js"
+  }
+]);
